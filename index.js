e18b00851b5255847ad523f0bd239f4a9bbd9caa
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-08-23 13:09:05 -0300

    exibição dos paises

---
+++ index.js
@@ -0,0 +1,22 @@
+let paisesSection = document.querySelector(".paises-section")
+async function carregarDados() {
+    const response = await fetch("./paises.json");
+    const dados = await response.json();
+
+    console.log(dados);
+    console.log(dados[0].nome_pais);
+    dados.forEach(pais => {
+        paisesSection.innerHTML+= `
+                <div class="container">
+                    <img class="bandeira-pais" src="${pais.bandeira_pais}">
+                    <h2>${pais.nome_pais}</h2>
+                    <p>Capital: <strong>${primeiraMaiuscula(pais.capital[0])}</strong></p>
+                </div>
+        `
+    });
+}
+carregarDados();
+function primeiraMaiuscula(str) {
+  if (!str) return "";
+  return str[0].toUpperCase() + str.slice(1);
+}
